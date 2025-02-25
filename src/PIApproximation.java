9914a61e770e2f446101bd79759cf5d1f08dc0da
Stephanie <stephanie.hu2@student.tdsb.on.ca>
2025-02-25 11:36:20 -0500

    git commit

---
+++ src/PIApproximation.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class PIApproximation {
+    public static void main(String[] args) throws Exception {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("\n\n" + "Please enter an n value: ");
+        int n = keyboard.nextInt();
+        keyboard.close();
+
+        
+    }
+}
